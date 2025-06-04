41cca3134a765cfbd4a6d21562f765c96c900268
Raj Ranjan <rranjan01234@gmail.com>
2025-06-04 13:27:02 +0530

    fix rpc imports

--- rpc/src/methods/index/index.ts
+++ rpc/src/methods/index/index.ts
@@ -1,39 +1,39 @@
-export type { getContainerByID } from "./getContainerByID.js";
+export { getContainerByID } from "./getContainerByID.js";
 export type {
   GetContainerByIDErrorType,
   GetContainerByIDParameters,
   GetContainerByIDReturnType,
 } from "./types/getContainerByID.js";
 
-export type { getContainerByIndex } from "./getContainerByIndex.js";
+export { getContainerByIndex } from "./getContainerByIndex.js";
 export type {
   GetContainerByIndexErrorType,
   GetContainerByIndexParameters,
   GetContainerByIndexReturnType,
 } from "./types/getContainerByIndex.js";
 
-export type { getContainerRange } from "./getContainerRange.js";
+export { getContainerRange } from "./getContainerRange.js";
 export type {
   GetContainerRangeErrorType,
   GetContainerRangeParameters,
   GetContainerRangeReturnType,
 } from "./types/getContainerRange.js";
 
-export type { getIndex } from "./getIndex.js";
+export { getIndex } from "./getIndex.js";
 export type {
   GetIndexErrorType,
   GetIndexParameters,
   GetIndexReturnType,
 } from "./types/getIndex.js";
 
-export type { getLastAccepted } from "./getLastAccepted.js";
+export { getLastAccepted } from "./getLastAccepted.js";
 export type {
   GetLastAcceptedErrorType,
   GetLastAcceptedParameters,
   GetLastAcceptedReturnType,
 } from "./types/getLastAccepted.js";
 
-export type { isAccepted } from "./isAccepted.js";
+export { isAccepted } from "./isAccepted.js";
 export type {
   IsAcceptedErrorType,
   IsAcceptedParameters,
